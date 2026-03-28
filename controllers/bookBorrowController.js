4c75252309d1d302026a1e6fc269a183940a28f9
Davsaltoby <davsaltobi@gmail.com>
2026-03-28 18:39:49 +0100

    create validation and response for book borrowing

--- controllers/bookBorrowController.js
+++ controllers/bookBorrowController.js
@@ -1,5 +1,30 @@
-const borrowBook = (req, res) => {
-  res.status(200).json({ message: "Book Borrow API Working" });
+const borrowBook = async (req, res) => {
+  const { studentId, attendantId, returnDate } = req.body;
+  try {
+    const bookData = {
+      status: "OUT",
+      borrowedBy: studentId,
+      issuedBy: attendantId,
+      returnDate,
+    };
+    const borrowedBook = await req.book.set(bookData).save();
+
+    await borrowedBook.populate([
+      { path: "authors", select: "name bio" },
+      { path: "borrowedBy", select: "name email" },
+      { path: "issuedBy", select: "name staffId" },
+    ]);
+
+    res.status(200).json({
+      ok: true,
+      message: "your request to borrow this book is successful",
+      data: borrowedBook,
+    });
+  } catch (err) {
+    res.status(500).json({ error: { message: "Cannot borrow book" } });
+
+    console.log(err.message);
+  }
 };
 
 export default borrowBook;
--- middleware/bookValidation/validateBorrowReturnBook.js
+++ middleware/bookValidation/validateBorrowReturnBook.js
@@ -1,18 +1,54 @@
+import Book from "../../models/bookModel.js";
+import mongoose from "mongoose";
+import Student from "../../models/studentModel.js";
+import LibraryAttendant from "../../models/libraryAttendantModel.js";
+
 export const validateBorrowBook = async (req, res, next) => {
-  const { borrowedBy, issuedBy } = req.body;
-  if (!borrowedBy || !issuedBy) {
+  const { studentId, attendantId, returnDate } = req.body;
+  if ((!studentId || !attendantId, !returnDate)) {
     return res
       .status(400)
       .json({ error: { message: "All fields are required" } });
   }
 
-  const invalidIds = [borrowedBy, issuedBy].filter(
+  const invalidIds = [studentId, attendantId].filter(
     (id) => !mongoose.Types.ObjectId.isValid(id),
   );
   if (invalidIds.length > 0) {
     return res.status(400).json({
-      erorr: { message: `Invalid Id format ${invalidIds.join(",")}` },
+      erorr: { message: `Invalid Id format ${invalidIds.join(", ")}` },
     });
   }
+
+  const isValidBookId = mongoose.Types.ObjectId.isValid(req.params.id);
+  if (!isValidBookId) {
+    return res
+      .status(400)
+      .json({ error: { message: "invalid book id format" } });
+  }
+
+  const book = await Book.findById(req.params.id);
+  if (!book) {
+    return res.status(404).json({ error: { message: "Book not found" } });
+  }
+
+  if (book.status !== "IN") {
+    return res
+      .status(409)
+      .json({ ok: false, message: "Book is currently unavailable" });
+  }
+
+  const student = await Student.findById(studentId);
+  if (!student) {
+    return res.status(404).json({ error: { message: "student not found" } });
+  }
+
+  const attendant = await LibraryAttendant.findById(attendantId);
+  if (!attendant) {
+    return res.status(404).json({ error: { message: "attendant not found" } });
+  }
+
+  req.book = book;
+
   next();
 };
